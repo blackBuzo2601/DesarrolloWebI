636622ba68dfc17617182e96b55865884a010ba2
blackBuzo2601 <elianpro58@gmail.com>
2025-02-10 16:13:07 -0800

    Informacion importante

--- 02IntroTypescript/src/temas/destructury.ts
+++ 02IntroTypescript/src/temas/destructury.ts
@@ -1,5 +1,40 @@
 const persona = {
   nombre: "saul",
-  apellido: "aparicio",
+  edad: 20,
   apodo: "saulillo",
 };
+
+//PascalCase para interfaces
+interface CrearNuevoHeroe {
+  nombre: string;
+  edad: number;
+  apodo: string;
+  poder?: string;
+}
+
+const Elian = {
+  nombre: "elian",
+  edad: 23,
+  apodo: "buzo",
+  poder: "estudiar",
+};
+
+const crearNuevoHeroe = ({ nombre, edad, apodo, poder }: CrearNuevoHeroe) => ({
+  id: 1234,
+  nombre: nombre,
+  edad: edad,
+  apodo: apodo,
+  poder: poder ?? "No tiene poder", //este es un operador ternario mas sencillo
+});
+
+console.log(crearNuevoHeroe({ ...persona, poder: "Jugar" }));
+
+/*
+const objeto:Heroe={}
+
+//repaso destructuring
+const { nombre, edad, apodo, poder: "Brincar" } = persona;
+
+//Otra  forma de acceder a las propiedades
+console.log({ nombre }); //{nombre: "saul"} imprime la key-value por asi decirlo
+ */
