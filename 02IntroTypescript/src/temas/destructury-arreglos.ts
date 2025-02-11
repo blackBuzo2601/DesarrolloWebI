79e8ebd92d44221590babec7e8c713b5c304c1f5
blackBuzo2601 <elianpro58@gmail.com>
2025-02-10 16:40:38 -0800

    explicacion con arreglos

--- 02IntroTypescript/src/temas/destructury-arreglos.ts
+++ 02IntroTypescript/src/temas/destructury-arreglos.ts
@@ -1,1 +1,13 @@
-console.log("Hola desde arreglos");
+const personajes = ["Gato con botas", "Lulú", "Shrek", "Zunni"];
+
+const [, , , , d = "Rafita"] = personajes;
+
+const regresarArreglo = (): (number | string)[] => {
+  return [1234, "ABCD"];
+};
+
+//como regresarArreglo devuelveu
+const [numeros, letra] = regresarArreglo();
+
+console.log(numeros);
+console.log(letra);
