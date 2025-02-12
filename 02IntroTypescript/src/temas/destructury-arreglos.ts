b20cf0372c10ec40196701fd6fb504a94f5653ee
blackBuzo2601 <elianpro58@gmail.com>
2025-02-12 15:50:46 -0800

    ejercicio pequeño con filter

--- 02IntroTypescript/src/temas/destructury-arreglos.ts
+++ 02IntroTypescript/src/temas/destructury-arreglos.ts
@@ -4,8 +4,9 @@ const personajes = ["Gato con botas", "Lulú", "Shrek", "Zunni"];
 //al hacer la desestructuracion, como es en el ejemplo siguiente:
 const [, , , , d = "Rafita"] = personajes;
 
-//es importante definir cuando la función retorna o nada, o en este caso
-//un arreglo con "tipos especificos":
+//es importante definir cuando la función retorna  NADA (void), o en este caso
+//un arreglo con "tipos especificos", como lo hacemos en la linea 9, para poder retornar
+//un arreglo con ese tipado
 const regresarArreglo = (): (number | string)[] => {
   return [1234, "hola"];
 };
--- 02IntroTypescript/src/temas/imp-exp.ts
+++ 02IntroTypescript/src/temas/imp-exp.ts
@@ -1,1 +1,14 @@
-console.log("hola peter");
+//cuando importamos, normalmente se hace con un (alias)
+import heroes, { owner, saludar } from "../data/heroes";
+
+const obtenerHeroeId = (id: number) => {
+  const encontrado = heroes.find((hero) => hero.id === id);
+  return encontrado ? `Se encontro ${encontrado.nombre}` : "No se encontró";
+};
+
+const obtenerPorOwner = (owner: string) => {
+  return heroes.filter((elemento) => elemento.owner === owner);
+};
+
+console.log(obtenerHeroeId(5));
+console.log(obtenerPorOwner("Marvel"));
