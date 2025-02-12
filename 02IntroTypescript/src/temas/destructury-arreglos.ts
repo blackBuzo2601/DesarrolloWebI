c8245e31b68da23d48244fa6829488903f5a448b
blackBuzo2601 <elianpro58@gmail.com>
2025-02-12 15:18:03 -0800

    Importacion y exportacion, y notas sobre como agregar elementos al desestructurar un arreglo

--- 02IntroTypescript/src/temas/destructury-arreglos.ts
+++ 02IntroTypescript/src/temas/destructury-arreglos.ts
@@ -1,12 +1,15 @@
 const personajes = ["Gato con botas", "Lulú", "Shrek", "Zunni"];
 
+//es posible, hacer una desestructuracion y tambien agregar un nuevo elemento
+//al hacer la desestructuracion, como es en el ejemplo siguiente:
 const [, , , , d = "Rafita"] = personajes;
 
+//es importante definir cuando la función retorna o nada, o en este caso
+//un arreglo con "tipos especificos":
 const regresarArreglo = (): (number | string)[] => {
-  return [1234, "ABCD"];
+  return [1234, "hola"];
 };
 
-//como regresarArreglo devuelveu
 const [numeros, letra] = regresarArreglo();
 
 console.log(numeros);
