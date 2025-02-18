fed17e449301e68ac18069ee869ee34e5730cbff
blackBuzo2601 <elianpro58@gmail.com>
2025-02-17 17:54:20 -0800

    m

--- 02IntroTypescript/src/temas/08fetch.ts
+++ 02IntroTypescript/src/temas/08fetch.ts
@@ -1,10 +1,12 @@
 import { type GIFResponse } from "../interface/gif.response";
 //recordemos que con type, definimos que es un tipado, y no es una clase etc.
 
-//apiKey
+//la key es la que nos proporcionó Giphy.com
 const apiKey = "5Sq3M7oGhVkPxVGXDGB5L1yWfmR0cmKc";
 //en las promesas, podemos colocar multiples ".then"
-//en el ejemplo siguiente, la respuesta la transformamos a un json,
+//usamos fetch, y ponemos el endpoint correspondiente para generar gifs aleatoriamente
+//ya finalmente colocamos la api_key en la url: api_key=apiKey, solo que aqui usamos
+//template strings
 
 fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
   .then((resp) => resp.json()) //respuesta en forma de json
